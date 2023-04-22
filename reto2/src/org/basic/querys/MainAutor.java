42d8ce893060949e04c9f2e4e01eb9779c7ed496
agustinrp89 <51332295+agustinrp89@users.noreply.github.com>
2023-04-22 16:12:09 -0300

    Update MainAutor.java

--- reto2/src/org/basic/querys/MainAutor.java
+++ reto2/src/org/basic/querys/MainAutor.java
@@ -1,5 +1,4 @@
 package org.basic.querys;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -12,9 +11,9 @@ public class MainAutor {
         HttpClient client = HttpClient.newHttpClient();
         String url = "https://serpapi.com/search.json?engine=google_scholar_author";
         String apiKey = "keyApi"; 
-        String autor_id = "qc6CJjYAAAAJ";
+        String autor_id = "";
         String hl = "en";
-        int numResults = 10;
+        int numResults = ;
 
       
         String requestUrl = url + "&author_id=" + autor_id + "&hl=" + hl + "&num=" + numResults + "&api_key=" + apiKey;
